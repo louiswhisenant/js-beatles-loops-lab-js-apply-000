7cf9b0fea81eafd4e526ce39bc63647cb6e0b8bb
Louis Whisenant <louiswhisenant@gmail.com>
2019-12-19 20:01:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,6 @@ function theBeatlesPlay() {
 
 
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive",
-              "He was never a vegetarian",
-              "He was a choir boy and boy scout",
-              "He hated the sound of his own voice"];
   var shoutFacts = [];
   var i = 0;
   while (i < facts.length) {
